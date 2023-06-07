692281d1f9bfb3ca6708fb91ae0709c55640dc14
janesz315 <borosuveg6@gmail.com>
2023-06-07 19:56:49 +0200

    upgrade in progress...

--- client/my.js
+++ client/my.js
@@ -27,10 +27,41 @@ class Cartoon {
   }
 }
 
+class Country {
+  constructor(
+    name = null,
+    
+    
+  ) {
+    this.name = name;
+    
+
+  }
+}
+
+class Creator {
+  constructor(
+    name = null,
+    
+    
+  ) {
+    this.name = name;
+    
+
+  }
+}
+
 let editableCartoon = new Cartoon();
+let editableCountry = new Country();
+let editableCreator = new Creator();
+
 let state = "view";
 let selectedCartoonId = null;
 let loadedCartoonId = null;
+let selectedCountryId = null;
+let loadedCountryId = null;
+let selectedCreatorId = null;
+let loadedCreatorId = null;
 
 function getHome() {
     let htmlElement = `
@@ -104,8 +135,8 @@ async function getTable() {
                       <button type="button" 
                           class="btn btn-outline-danger btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickDeleteButton(${cartoon.id})"
-                      >
+                          onclick="onClickDeleteButton(${cartoon.id})">
+                          
                           <i class="bi bi-trash3-fill"></i>
                       </button>
                       <button type="button" 
@@ -154,12 +185,13 @@ async function getTable2(params) {
                   <th>
                       <button type="button" class="btn btn-outline-success btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickNewButton()"
+                          onclick="onClickNewButton2()"
                       >
                           Add a country!
                       </button>
                   </th>
                   <th>Name</th>
+                  <th>Id</th>
                   
                 </tr>
           </thead>
@@ -175,19 +207,20 @@ async function getTable2(params) {
                       <button type="button" 
                           class="btn btn-outline-danger btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickDeleteButton(${country.id})"
+                          onclick="onClickDeleteButton2(${country.id})"
                       >
                           <i class="bi bi-trash3-fill"></i>
                       </button>
                       <button type="button" 
                           class="btn btn-outline-warning btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickEditButton(${country.id})"
+                          onclick="onClickEditButton2(${country.id})"
                       >
                       <i class="bi bi-pencil-fill"></i>
                       </button>
                   </td>
                   <td>${country.name}</td>
+                  <td>${country.id}</td>
                   
               </tr>
           `;
@@ -219,12 +252,13 @@ async function getTable3(params) {
                   <th>
                       <button type="button" class="btn btn-outline-success btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickNewButton()"
+                          onclick="onClickNewButton3()"
                       >
                           Add a creator!
                       </button>
                   </th>
                   <th>Name</th>
+                  <th>Id</th>
                   
                 </tr>
           </thead>
@@ -240,19 +274,20 @@ async function getTable3(params) {
                       <button type="button" 
                           class="btn btn-outline-danger btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickDeleteButton(${creator.id})"
+                          onclick="onClickDeleteButton3(${creator.id})"
                       >
                           <i class="bi bi-trash3-fill"></i>
                       </button>
                       <button type="button" 
                           class="btn btn-outline-warning btn-sm"
                           data-bs-toggle="modal" data-bs-target="#modalCard"
-                          onclick="onClickEditButton(${creator.id})"
+                          onclick="onClickEditButton3(${creator.id})"
                       >
                       <i class="bi bi-pencil-fill"></i>
                       </button>
                   </td>
                   <td>${creator.name}</td>
+                  <td>${creator.id}</td>
                   
               </tr>
           `;
@@ -536,13 +571,83 @@ async function onClickNewButton() {
 
   modalContent.innerHTML = htmlElement;
 }
+async function onClickNewButton3() {
+  state = "new";
+  modalTitle.innerHTML = "A new record of a creator";
+  buttonShowHide("saveButton", true);
+  const url = "http://localhost:3000/creators";
+  const response = await fetch(url);
+  const data = await response.json();
+  const cartoon = data.data
+  
+  
+
+  let htmlElement = `
+    <div class="col-12">
+        <label for="name" class="form-label">The creator name:</label>
+        <input type="text" class="form-control" id="name">
+    
+    
+    
+    `;
+  
+  
+  
+
+  //vége
+  htmlElement += `</div>`;
+
+  modalContent.innerHTML = htmlElement;
+}
+async function onClickNewButton2() {
+  state = "new";
+  modalTitle.innerHTML = "A new record of a country";
+  buttonShowHide("saveButton", true);
+  const url = "http://localhost:3000/countries";
+  const response = await fetch(url);
+  const data = await response.json();
+  const cartoon = data.data
+  
+  
+
+  let htmlElement = `
+    <div class="col-12">
+        <label for="name" class="form-label">The country's name:</label>
+        <input type="text" class="form-control" id="name">
+    
+    
+    
+    `;
+  
+  
+  
+
+  //vége
+  htmlElement += `</div>`;
+
+  modalContent.innerHTML = htmlElement;
+}
 
 function onClickDeleteButton(id) {
   state = "delete";
-  modalTitle.innerHTML = "Car deletion";
+  modalTitle.innerHTML = "Cartoon deletion";
   modalContent.innerHTML = "Do you really want that?";
   buttonShowHide("yesButton", true);
   selectedCartoonId = id;
+}
+function onClickDeleteButton2(id) {
+  state = "delete";
+  modalTitle.innerHTML = "country deletion";
+  modalContent.innerHTML = "Do you really want that?";
+  buttonShowHide("yesButton", true);
+  selectedCountryId = id;
+}
+function onClickDeleteButton3(id) {
+  state = "delete";
+  modalTitle.innerHTML = "creator deletion";
+  modalContent.innerHTML = "Do you really want that?";
+  buttonShowHide("yesButton", true);
+  selectedCreatorId = id;
 }
 
 async function onClickEditButton(id) {
@@ -614,6 +719,75 @@ async function onClickEditButton(id) {
   
   selectedCartoonId = id;
 }
+async function onClickEditButton2(id) {
+  //sofőrök beolvasása -> drivers
+  let url = "http://localhost:3000/countries";
+  let response = await fetch(url);
+  let data = await response.json();
+  
+  state = "edit";
+  modalTitle.innerHTML = "You are currently editing the country";
+  buttonShowHide("saveButton", true);
+
+  let htmlElement = `
+  <div class="col-12">
+        <label for="name" class="form-label">The country name:</label>
+        <input type="text" class="form-control" id="name">
+    
+  `;
+  // ciklus
+  
+
+  //vége
+  htmlElement += `</div>`;
+  modalContent.innerHTML = htmlElement;
+
+  //a kifálaszottt arutó -> car
+  url = `http://localhost:3000/country/${id}`;
+  response = await fetch(url);
+  data = await response.json();
+  const country = data.data[0];
+
+  document.getElementById("name").value = country.name;
+  
+  
+  selectedCountryId = id;
+}
+
+async function onClickEditButton3(id) {
+  //sofőrök beolvasása -> drivers
+  let url = "http://localhost:3000/creators";
+  let response = await fetch(url);
+  let data = await response.json();
+  
+  state = "edit";
+  modalTitle.innerHTML = "You are currently editing the creator";
+  buttonShowHide("saveButton", true);
+
+  let htmlElement = `
+  <div class="col-12">
+        <label for="name" class="form-label">The cartoon name:</label>
+        <input type="text" class="form-control" id="name">
+    
+  `;
+  // ciklus
+  
+
+  //vége
+  htmlElement += `</div>`;
+  modalContent.innerHTML = htmlElement;
+
+  //a kifálaszottt arutó -> car
+  url = `http://localhost:3000/creators/${id}`;
+  response = await fetch(url);
+  data = await response.json();
+  const creator = data.data[0];
+
+  document.getElementById("name").value = creator.name;
+  
+  selectedCreatorId = id;
+}
+
 
 async function onClickSaveButton() {
   buttonShowHide("saveButton", false);
@@ -674,6 +848,89 @@ async function onClickSaveButton() {
   //lássuk hogy bővült a táblázat
   getTable();
 }
+async function onClickSaveButton2() {
+  buttonShowHide("saveButton", false);
+  buttonShowHide("yesButton", false);
+
+  //olvassuk ki az űrlap adatait
+  editableCountry.name = document.getElementById("name").value;
+  
+
+  
+  
+  
+  
+  
+  
+  if (state === "new") {
+    const url = "http://localhost:3000/countries";
+    //obj to json konverzió
+    const body = JSON.stringify(editableCountry);
+    const config = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: body,
+    };
+    const response = await fetch(url, config);
+    console.log(editableCountry);
+  } else if (state === "edit") {
+    const url = `http://localhost:3000/country/${selectedCountryId}`;
+    const body = JSON.stringify(editableCountry);
+    const config = {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: body,
+    };
+    const response = await fetch(url, config);
+  }
+
+  //lássuk hogy bővült a táblázat
+  getTable();
+}
+async function onClickSaveButton3() {
+  buttonShowHide("saveButton", false);
+  buttonShowHide("yesButton", false);
+
+  //olvassuk ki az űrlap adatait
+  editableCreator.name = document.getElementById("name").value;
+  
+
+  
+  if (state === "new") {
+    const url = "http://localhost:3000/creators";
+    //obj to json konverzió
+    const body = JSON.stringify(editableCreator);
+    const config = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: body,
+    };
+    const response = await fetch(url, config);
+    console.log(editableCreator);
+  } else if (state === "edit") {
+    const url = `http://localhost:3000/creators/${selectedCreatorId}`;
+    const body = JSON.stringify(editableCreator);
+    const config = {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: body,
+    };
+    const response = await fetch(url, config);
+  }
+
+  //lássuk hogy bővült a táblázat
+  getTable();
+}
+
+
 
 //ide építjük be törlés ajax kérést
 async function onClickYesButton() {
