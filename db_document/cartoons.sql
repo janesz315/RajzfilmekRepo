2f87039490665a88f65a27cb57184156eda300f5
janesz315 <borosuveg6@gmail.com>
2023-06-16 13:49:17 +0200

    今完全になった。Everything is working.

--- db_document/cartoons.sql
+++ db_document/cartoons.sql
@@ -10,11 +10,27 @@ select randomDate();
 select randomDate2('1983-01-01','1989-10-20');
 
 
+#get cartoons
+SELECT cartoons.id, cartoons.name,numberOfSeasons,numberOfEpisodes,countries.name countriesId,creators.name creatorsId,runningTime,DATE_FORMAT(AiringStart, '%Y. %m. %d') AiringStart, DATE_FORMAT(AiringEnd, '%Y. %m. %d') AiringEnd FROM cartoons
+  INNER JOIN countries on countriesId = countries.id
+  INNER JOIN creators on creatorsId = creators.id
+;
 
-SELECT co.id, co.name ,cr.id , cr.name FROM countries co
-    INNER JOIN creators cr on co.id = cr.id
-    order by cr.name, co.name;
 
-SELECT countries.name from countries
-  outer join creators
-  
+  ;
+#get cartoons/:id
+SELECT cartoons.id, cartoons.name,numberOfSeasons,numberOfEpisodes,countries.name countriesName, countries.id countriesId,creators.name creatorsName, creators.id creatorsId, runningTime,DATE_FORMAT(AiringStart, '%Y-%m-%d') AiringStart, DATE_FORMAT(AiringEnd, '%Y-%m-%d') AiringEnd FROM cartoons
+    INNER JOIN countries on countriesId = countries.id
+    INNER JOIN creators on creatorsId = creators.id
+    WHERE cartoons.id = 6
+  ;
+
+
+#countriesAbc
+SELECT name, id from countries
+  order by name;
+
+#creatorsAbc
+
+SELECT name, id from creators
+  order by name;
